b686db06c20c099feec9147e37b9f58941e99574
Jonathan Coe <info@joncoe.com>
2019-01-27 17:00:58 -0500

    refactor quotes as imported module

--- src/components/FuseWelcome.js
+++ src/components/FuseWelcome.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {TweenMax} from 'gsap';
 import {Quad} from 'gsap/src/uncompressed/easing/EasePack';
 
+import quotes from './data/quotes';
+
 import {
   Link
 } from 'react-router-dom';
@@ -16,38 +18,7 @@ class FuseWelcome extends Component {
     this.quotesArr = [];
 
     this.state = {
-      quotes: [
-        {
-          quote: 'Twenty two year old Richie Hawtin releases his first full solo album and it\'s a corker. Including the seminal \'F.U.\' and \'Substance Abuse\' this is techno, trance, acid and ambience of the highest quality.',
-          media: '- Jockey Slut,',
-          date: 'July/August 1993'
-        },
-        {
-          quote: '…’Dimension Intrusion’ is an essential purchase for space-freaks and anyone who enjoys a good hour or two of empirical sonic art.',
-          media: '- Melody Maker,',
-          date: 'June 1993'
-        },
-        {
-          quote: 'The Train Abuse mix is a floor friendly Armani style repetitive drum pattern, hi-hats and bass. Simple and effective.',
-          media: '- Mark and Josh, Mixmag Update,',
-          date: 'June 1993'
-        },
-        {
-          quote: 'Reviewing this album track is not applicable as this is a journey of extremely futuristic and experimental music that as a whole is another very well written chapter in the great book of techno.',
-          media: '- Mark and Josh, Mixmag Update,',
-          date: 'May 1993'
-        },
-        {
-          quote: '…there’s still a defiant, solitary, almost punky attitude at work here: strange haunting, compulsive. If you don’t like it, it’s your loss.',
-          media: '- Rupert Howe, Select,',
-          date: 'July 1993'
-        },
-        {
-          quote: 'Hawtin’s name has been synonymous with techno’s outer rings and inner expansion through the 90’s. This is why.',
-          media: '- Kris Needs, NM',
-          date: ''
-        }
-      ]
+      quotes: quotes
     }
   }
 
@@ -83,34 +54,26 @@ class FuseWelcome extends Component {
 
   render() {
 
-    return (
-      <div ref={div => this.parentElement = div}>
-        
-        <section className="fuse-quote-rotation">
-          <ul className="list-unstyled">
-            
-            {/* render quotes */}
-            {this.state.quotes.map((quote, i) => {
-              return (
-                <li ref={li => this.quotesArr[i] = li} key={i}>
-                  <p>
-                    <q>
-                      {quote.quote}
-                    </q><br />
-                      {quote.media} {quote.date}
-                  </p>
-                </li>
-              );
-            })}
+    return (   
+      <section className="fuse-quote-rotation">
+        <ul className="list-unstyled">
           
-          </ul>
-        </section>
-
-        <nav>
-            
-        </nav>
-
-      </div>
+          {/* render quotes */}
+          {this.state.quotes.map((quote, i) => {
+            return (
+              <li ref={li => this.quotesArr[i] = li} key={i}>
+                <p>
+                  <q>
+                    {quote.quote}
+                  </q><br />
+                    {quote.media} {quote.date}
+                </p>
+              </li>
+            );
+          })}
+        
+        </ul>
+      </section>
     );
   }
 }
---
+++ src/components/data/quotes.js
@@ -0,0 +1,34 @@
+const quotes = [
+    {
+      quote: 'Twenty two year old Richie Hawtin releases his first full solo album and it\'s a corker. Including the seminal \'F.U.\' and \'Substance Abuse\' this is techno, trance, acid and ambience of the highest quality.',
+      media: '- Jockey Slut,',
+      date: 'July/August 1993'
+    },
+    {
+      quote: '…’Dimension Intrusion’ is an essential purchase for space-freaks and anyone who enjoys a good hour or two of empirical sonic art.',
+      media: '- Melody Maker,',
+      date: 'June 1993'
+    },
+    {
+      quote: 'The Train Abuse mix is a floor friendly Armani style repetitive drum pattern, hi-hats and bass. Simple and effective.',
+      media: '- Mark and Josh, Mixmag Update,',
+      date: 'June 1993'
+    },
+    {
+      quote: 'Reviewing this album track is not applicable as this is a journey of extremely futuristic and experimental music that as a whole is another very well written chapter in the great book of techno.',
+      media: '- Mark and Josh, Mixmag Update,',
+      date: 'May 1993'
+    },
+    {
+      quote: '…there’s still a defiant, solitary, almost punky attitude at work here: strange haunting, compulsive. If you don’t like it, it’s your loss.',
+      media: '- Rupert Howe, Select,',
+      date: 'July 1993'
+    },
+    {
+      quote: 'Hawtin’s name has been synonymous with techno’s outer rings and inner expansion through the 90’s. This is why.',
+      media: '- Kris Needs, NM',
+      date: ''
+    }
+  ]
+
+export default quotes;
