3b12bffb07f078251f51f060d2e580d38bdf142e
Jonathan Coe <info@joncoe.com>
2019-01-26 19:36:04 -0500

    quotes

--- src/components/Fuse.js
+++ src/components/Fuse.js
@@ -1,42 +1,37 @@
 import React, { Component } from 'react';
 
+import FuseWelcome from './FuseWelcome';
 import Albums from './Albums';
 import AlbumDetails from './AlbumDetails';
 
 
 import {
-  // BrowserRouter as Router,
-  // Redirect,
   Route,
   Link,
-  // Switch
+  Switch
 } from 'react-router-dom';
 
 class Fuse extends Component {
   render() {
     return (
       <div>
-        <header className="App-header">
-          
-          <nav>
-            <h1><Link to="/albums">f.u.s.e. dimensions</Link></h1>
-          </nav>
-        </header>
-
-        <Route path="/albums" component={Albums} />
 
 
-        <Route 
-          path="/album/:albumName"
-        
-          render={(params) => {
+        <Switch>
 
-            return <AlbumDetails {...params} />
-
-          }}
+          <Route path="/" component={FuseWelcome} />
+          <Route path="/albums" component={Albums} />
 
 
-        />
+          <Route 
+            path="/album/:albumName"
+            render={(params) => {
+              return <AlbumDetails {...params} />
+            }}
+
+          />
+
+        </Switch>
 
 
         
---
+++ src/components/FuseWelcome.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+
+import {
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
+
+class FuseWelcome extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <nav>
+            <h1><Link to="/albums">f.u.s.e. dimensions</Link></h1>
+          </nav>
+        </header>
+
+        <section className="fuse-quote-rotation">
+          <ul className="list-unstyled">
+            <li>
+              <p>
+                <q>
+                  Twenty two year old Richie Hawtin releases his first full solo album
+                  and it’s a corker. Including the seminal ‘F.U.’ and ‘Substance Abuse’
+                  this is techno, trance, acid and ambience of the highest quality.
+                </q><br />
+                - Jockey Slut,
+                July/August 1993
+              </p>
+            </li>
+
+            <li>
+              <p>
+                <q>
+                  …’Dimension Intrusion’ is an essential purchase for space-freaks and anyone who enjoys a good hour or two of empirical sonic art.
+                </q><br />
+                - Melody Maker,
+                June 1993
+              </p>
+            </li>
+            <li>
+              <p>
+                <q>
+                  The Train Abuse mix is a floor friendly Armani style repetitive drum pattern, hi-hats and bass. Simple and effective.
+                </q><br />
+                - Mark and Josh, Mixmag Update,
+                September 1993
+              </p>
+            </li>
+            <li>
+              <p>
+                <q>
+                  Reviewing this album track is not applicable as this is a journey of extremely futuristic and experimental music that as a whole is another very well written chapter in the great book of techno.
+                </q><br />
+                - Mark and Josh, Mixmag Update,
+                May 1993
+              </p>
+            </li>
+            <li>
+              <p>
+                <q>
+                  …there’s still a defiant, solitary, almost punky attitude at work here: strange haunting, compulsive. If you don’t like it, it’s your loss.
+                </q><br />
+                - Rupert Howe, Select,
+                July 1993
+              </p>
+            </li>
+            <li>
+              <p>
+                <q>
+                  Hawtin’s name has been synonymous with techno’s outer rings and inner expansion through the 90’s. This is why.
+                </q><br />
+                - Kris Needs, NM
+              </p>
+            </li>
+          </ul>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default FuseWelcome;
