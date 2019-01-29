bb10983f05cb2918dc15d341ad7181f21bf82821
Jonathan Coe <info@joncoe.com>
2019-01-28 21:20:14 -0500

    add icons, shop buttons

---
+++ src/components/ShoppingCartButton.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCartArrowDown);
+
+class ShoppingCartButton extends Component {
+
+  render() {
+    return (
+      <a href={this.props.url} className="icon-button">
+        <span className="icon-button-container">
+          <FontAwesomeIcon icon="cart-arrow-down" size="lg"/>
+          <span className="icon-button-text">{this.props.urlText}</span>
+        </span>
+      </a>
+    );
+  }
+}
+
+export default ShoppingCartButton;
