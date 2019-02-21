a9f2d8844d6c5da00eadb0ff529d1fdc5b29c120
joncoe <info@joncoe.com>
2019-02-20 19:54:04 -0500

    Add white bg for album text on Albums page.

--- src/components/Fuse.js
+++ src/components/Fuse.js
@@ -14,6 +14,8 @@ import {
 } from 'react-router-dom';
 import NotFound404 from './NotFound404';
 
+import {TweenMax} from 'gsap';
+
 import albumData from './data/albumdata';
 
 import {
@@ -49,6 +51,34 @@ class Fuse extends Component {
     // TweenMax.set(titleNode, {y: 200})
     // console.log(this.titleNode);
     // TweenMax.to(this.titleNode, 1, {y:200});
+
+    this.props.history.listen((location, action) => {
+      if(location.pathname === '/albums') {
+        this.changeSVGBackground();
+      }      
+    })
+
+    if (this.props.history.location.pathname === '/albums') {
+      this.changeSVGBackground();
+    }
+    
+  }
+
+  changeSVGBackground() {
+    console.log('change svg background');
+    TweenMax.to(this.path1, 2, {
+      attr:{
+        d:"M0 1273.284v157.322l1281.614-508.109L1483.5 1145.5l2891.775 285.106v-157.322L3344 1248l-1804.5-190-257.886-154z"
+       }
+
+    })
+
+    TweenMax.to(this.path2, 2, {
+      attr:{
+        d:"M0 1273.284v157.322l1281.614-508.109L1483.5 1145.5l2891.775 285.106v-157.322L3344 1248l-1804.5-190-257.886-154z"
+       }
+
+    })
   }
 
   selectAlbum(albumIndex) {
@@ -75,8 +105,12 @@ class Fuse extends Component {
 
       <svg width="100%" height="100%" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" id="svg-bg" preserveAspectRatio="none">
         <g fill="none" fillRule="evenodd">
-          <path fill="#FD0" d="M-394.386 772.014L-1676 1141.284v157.322l1281.614-508.109v-.001L389.748 931.52l2309.527 367.086v-157.322l-1028.268-63.779L389.748 886.166-394.386 772z" />
-          <path fill="#FFCB05" d="M-279 185.035L720.567 29.483l581.731-67.885L-265.288 28.31z" />
+          <path 
+          ref={div => this.path1 = div} 
+          fill="#FD0" d="M-394.386 772.014L-1676 1141.284v157.322l1281.614-508.109v-.001L389.748 931.52l2309.527 367.086v-157.322l-1028.268-63.779L389.748 886.166-394.386 772z" />
+          <path 
+          ref={div => this.path2 = div} 
+          fill="#FFCB05" d="M-279 185.035L720.567 29.483l581.731-67.885L-265.288 28.31z" />
         </g>
       </svg>
 
