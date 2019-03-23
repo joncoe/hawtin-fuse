0bd594c45583443289d662b2c478390209324861
joncoe <info@joncoe.com>
2019-03-23 11:48:09 -0400

    Update to text

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -131,22 +131,26 @@ class Albums extends Component {
 
                   <p>‘Dimensions’ is a limited edition vinyl box set celebrating the 25 years anniversary of Richie Hawtin’s original F.U.S.E. releases.</p>
 
-<p>The box set features re-releases of the classic albums ‘Dimension Intrusion’ and ‘Train-Tracs’, both including previously unreleased tracks. In addition it premiers the unreleased album ‘Computer Space’, completing the extended range of Hawtin’s recording persona in the formative year of 1993. All albums are re-mastered and pressed on white vinyl. Digital download codes are included. </p>
+                  <p>The box set features re-releases of the classic albums ‘Dimension Intrusion’ and ‘Train-Tracs’, both including previously unreleased tracks. In addition it premiers the unreleased album ‘Computer Space’, completing the extended range of Hawtin’s recording persona in the formative year of 1993. All albums are re-mastered and pressed on 180g white vinyl. Digital download codes are included. </p>
 
-<p>The three album covers are based upon original artworks by visual artist Matthew Hawtin, Richie’s brother, and are included in the box set in form of exclusive artist grade prints.</p>
+                  <p>The three album covers are based upon original artworks by visual artist Matthew Hawtin, Richie’s brother, and are included in the box set in form of exclusive artist grade prints.</p>
 
-<p>The booklet features a newly written essay by Matthew Hawtin alongside unpublished images and words by Richie Hawtin.</p>
+                  <p>The booklet features a newly written essay by Matthew Hawtin alongside unpublished images and words by Richie Hawtin.</p>
 
-<p>The box set itself was custom designed for the anniversary release by Matthew. Its design is based upon the original artwork that inspired the early albums and emphasizes the reciprocal inspiration between the brother’s creative work. </p>
+                  <p>The box set itself was custom designed for the anniversary release by Matthew. Its design is based upon the original artwork that inspired the early albums and emphasizes the reciprocal inspiration between the brother’s creative work. </p>
 
-<ul className="list-unstyled text-center album-features">
-  <li>1 custom made vinyl box set</li>
-  <li>8 Vinyl</li>
-  <li>digital download codes</li>
-  <li>3 prints</li>
-  <li>1 booklet</li>
-  <li>Limited Edition of 500</li>
-</ul>
+
+                  <ul className="list-unstyled text-center album-features">
+                    <li>Designed by Matthew Hawtin</li>
+                    <li>Bespoke hand-crafted box with magnetic closure, soft touch lamination and spot UV finish </li>
+                    <li>3 x artist prints of the original album covers, printed on 250gsm somerset velvet paper</li>
+                    <li>20pp book with soft touch lamination and spot UV cover </li>
+                    <li>8 x 180g white vinyl records</li>
+                    <li>1 x digital download card</li>
+                    <li>Limited edition of 500</li>
+                    <li>Available to pre-order now</li>
+                    <li>Release date: April 15, 2019</li>
+                  </ul>
 
                   </section>
 
